cb70a6edaf4ba8a5e47f4fd3d61b5d9b9a954734
ssmg <ssmg.sg@gmail.com>
2021-03-22 16:19:08 -0600

    Improve code Create format_guide_data function Create save_guide_to_database function Filter valid_df and invalid_df

--- main.py
+++ main.py
@@ -37,69 +37,85 @@ def capitalize_each_word_in_string(string: "str", delimiter: "str" = " ") -> "st
     return capitalized_string
 
 
+def format_guide_data(guide, df):
+    # Original data
+    id = df.loc[guide, "NumeroGuia"]
+    date = df.loc[guide, "Fecha"]
+    sender = df.loc[guide, "Remitente"]
+    addressee = df.loc[guide, "Destinatario"]
+    reference_1 = df.loc[guide, "Referencia 1"]
+    reference_2 = df.loc[guide, "Referencia 2"]
+    credit_code = df.loc[guide, "CCredito"]
+    status = df.loc[guide, "Estado"]
+    reason = df.loc[guide, "Motivo"]
+    destination = df.loc[guide, "Destino"]
+    received_by = df.loc[guide, "Recibido Por"]
+    received_date = df.loc[guide, "Fecha Recibido"]
+    received_time = df.loc[guide, "Recibido Hora"]
+    paid = False
+
+    # Modifications
+    id = df.loc[guide, "NumeroGuia"]
+    date = date.strftime("%d/%m/%Y")
+    sender = capitalize_each_word_in_string(sender, "-")
+    addressee = capitalize_each_word_in_string(addressee)
+    reference_1 = str(reference_1).lower() if type(
+        reference_1) == str else ""
+    reference_2 = str(reference_2).lower() if type(
+        reference_2) == str else ""
+    credit_code = str(int(credit_code))
+    status = str(status).lower()
+    reason = str(reason).lower() if type(
+        reason) == str else ""
+    destination = df.loc[guide, "Destino"]
+    received_by = capitalize_each_word_in_string(received_by)
+    received_date = received_date.strftime("%d/%m/%Y")
+    received_time = df.loc[guide, "Recibido Hora"]
+    paid = False
+
+    formatted_guide = {
+        "_id": id,
+        "date": date,
+        "sender": sender,
+        "addressee": addressee,
+        "reference 1": reference_1,
+        "reference 2": reference_2,
+        "credit code": credit_code,
+        "status": status,
+        "reason": reason,
+        "destination": destination,
+        "received by": received_by,
+        "received date": received_date,
+        "received time": received_time,
+        "paid": paid
+    }
+
+    return formatted_guide
+
+
+def save_guide_to_database(guide):
+    try:
+        guides_collection.insert_one(guide)
+        return True
+    except pymongo.errors.DuplicateKeyError:
+        return False
+
+
 @click.command()
 @click.argument("filename", type=click.Path(exists=True))
 def save_guides_into_database(filename):
     df = pd.read_excel(filename)
     df = df[0:len(df) - 1]  # To quit invalid field
-    df = df[(df["NumeroGuia"].str[1:3] == "DG")
-            | (df["Motivo"] == "DEVOLUCION")
-            | (df["Motivo"] == "FISCALIZACION")]
-    print(df)
+    valid_df = df[
+        (df["NumeroGuia"].str[1:3] != "DG")
+        & (df["Motivo"] != "DEVOLUCION")
+        & (df["Motivo"] != "FISCALIZACION")
+    ]
+    invalid_df = df[
+        (df["NumeroGuia"].str[1:3] == "DG")
+        | (df["Motivo"] == "DEVOLUCION")
+        | (df["Motivo"] == "FISCALIZACION")
+    ]
 
-    for guide in df.index:
-        # Original data
-        id = df.loc[guide, "NumeroGuia"]
-        date = df.loc[guide, "Fecha"]
-        sender = df.loc[guide, "Remitente"]
-        addressee = df.loc[guide, "Destinatario"]
-        reference_1 = df.loc[guide, "Referencia 1"]
-        reference_2 = df.loc[guide, "Referencia 2"]
-        credit_code = df.loc[guide, "CCredito"]
-        status = df.loc[guide, "Estado"]
-        reason = df.loc[guide, "Motivo"]
-        destination = df.loc[guide, "Destino"]
-        received_by = df.loc[guide, "Recibido Por"]
-        received_date = df.loc[guide, "Fecha Recibido"]
-        received_time = df.loc[guide, "Recibido Hora"]
-        paid = False
-
-        # Modifications
-        id = df.loc[guide, "NumeroGuia"]
-        date = date.strftime("%d/%m/%Y")
-        sender = capitalize_each_word_in_string(sender, "-")
-        addressee = capitalize_each_word_in_string(addressee)
-        reference_1 = str(reference_1).lower() if type(
-            reference_1) == str else ""
-        reference_2 = str(reference_2).lower() if type(
-            reference_2) == str else ""
-        credit_code = str(int(credit_code))
-        status = str(status).lower()
-        reason = str(reason).lower() if type(
-            reason) == str else ""
-        destination = df.loc[guide, "Destino"]
-        received_by = capitalize_each_word_in_string(received_by)
-        received_date = received_date.strftime("%d/%m/%Y")
-        received_time = df.loc[guide, "Recibido Hora"]
-        paid = False
-
-        guide_to_save = {
-            "_id": id,
-            "date": date,
-            "sender": sender,
-            "addressee": addressee,
-            "reference 1": reference_1,
-            "reference 2": reference_2,
-            "credit code": credit_code,
-            "status": status,
-            "reason": reason,
-            "destination": destination,
-            "received by": received_by,
-            "received date": received_date,
-            "received time": received_time,
-            "paid": paid
-        }
-    try:
-        guide_id = guides_collection.insert_one(guide_to_save).inserted_id
-    except pymongo.errors.DuplicateKeyError:
-        print("The guide already exists in the database.")
+    for guide in valid_df.index:
+        format_guide_data(guide, valid_df)
