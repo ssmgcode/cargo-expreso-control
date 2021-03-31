23e4fbf431815d65a5ad1fa6ac9cc7d8d4edb9e6
ssmg <ssmg.sg@gmail.com>
2021-03-30 21:02:26 -0600

    Add new entry point Change name to to generate scripts name

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='main',
-    version='0.1',
-    py_modules=['main'],
+    name="main",
+    version="0.1",
+    py_modules=["main"],
     install_requires=[
-        'Click',
+        "Click",
     ],
-    entry_points='''
+    entry_points="""
         [console_scripts]
-        main=main:save_guides_to_database
-    ''',
+        save-guides-to-database=main:save_guides_to_database
+        check-guides-paid=main:check_guides_paid
+    """,
 )
